aa003bffa4a6c545177c0d080821023b8613a624
Gabriele Armento <gabriele.armento@gmail.com>
2022-03-14 11:21:39 +0100

    chg folder structure

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+import pymt5adapter as _pymt5
+from .core import *
+
+__version__ = {
+    'betterMT5': '0.1',
+    'pymt5adapter': _pymt5.__version__,
+}
+
+__author__ = {
+    'betterMT5': 'Gabriele Armento',
+    'pymt5adapter': _pymt5.__author__
+}
