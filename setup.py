d5c4a0a11aedf91c7c07c55c4c38f8225a27c01c
965311532 <gabriele.armento@gmail.com>
2022-04-24 23:10:25 +0200

    hotfix missing modules

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="bettermt5",
-    version="0.9.1",
+    version="0.9.2",
     author="Gabriele Armento",
     author_email="contact@gabrielearmento.com",
     description="A simple MetaTrader5 wrapper",
